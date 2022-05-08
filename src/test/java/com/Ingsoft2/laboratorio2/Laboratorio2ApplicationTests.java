c1d93882a59355bc5b3789eba42e489cc9e843a1
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-05-07 20:18:49 -0500

    Init Commit

---
+++ src/test/java/com/Ingsoft2/laboratorio2/Laboratorio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ingsoft2.laboratorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
